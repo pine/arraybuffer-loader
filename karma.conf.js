c04bebccbd62c8a67cb9e89d4e9ad3d482510c01
Pine Mizune <pinemz@gmail.com>
2018-05-23 09:56:10 +0900

    fix(karma.conf.js): add ie10

--- karma.conf.js
+++ karma.conf.js
@@ -15,10 +15,16 @@ const customLaunchers = {
     platform: 'Windows 10',
     version: '47.0'
   },
-  sl_ie: {
+  sl_ie10: {
     base: 'SauceLabs',
     browserName: 'internet explorer',
     platform: 'Windows 7',
+    version: '10.0',
+  },
+  sl_ie11: {
+    base: 'SauceLabs',
+    browserName: 'internet explorer',
+    platform: 'Windows 10',
     version: '11.0',
   },
   sl_edge: {
