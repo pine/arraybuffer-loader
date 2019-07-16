f9b557598a925c8710681aa6a251287e682eaffb
Pine Mizune <pinemz@gmail.com>
2019-07-17 00:29:56 +0900

    fix: remove souce labs

--- karma.conf.js
+++ karma.conf.js
@@ -2,53 +2,6 @@
 
 const path = require('path')
 
-const customLaunchers = {
-  sl_chrome: {
-    base: 'SauceLabs',
-    browserName: 'chrome',
-    platform: 'Windows 10',
-    version: '51.0'
-  },
-  sl_firefox: {
-    base: 'SauceLabs',
-    browserName: 'firefox',
-    platform: 'Windows 10',
-    version: '47.0'
-  },
-  sl_ie10: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 7',
-    version: '10.0',
-  },
-  sl_ie11: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 10',
-    version: '11.0',
-  },
-  sl_edge: {
-    base: 'SauceLabs',
-    browserName: 'MicrosoftEdge',
-    platform: 'Windows 10',
-    version: '13.10586'
-  },
-  sl_safari: {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'OS X 10.11',
-    version: '9.0',
-  },
-}
-
-const isTravis   = process.env.TRAVIS === 'true'
-const isFirstJob = /\.1$/.test(process.env.TRAVIS_JOB_NUMBER)
-
-const browsers = ['PhantomJS']
-if (isTravis && isFirstJob) {
-  Array.prototype.push.apply(browsers, Object.keys(customLaunchers))
-}
-
 module.exports = function(config) {
 
   config.set({
@@ -68,7 +21,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: browsers,
+    browsers: ['PhantomJS'],
     customLaunchers: customLaunchers,
     singleRun: false,
 
